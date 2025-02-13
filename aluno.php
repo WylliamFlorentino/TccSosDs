fe979a01e029e82ed5d010e1aaa63e0c0afc9c31
wylliam <wylliamflorentino@gmail.com>
2025-02-12 22:30:26 -0300

    Erros alunos tirado

--- aluno.php
+++ aluno.php
@@ -2,26 +2,21 @@
 <?php 
 if( isset($_GET["pesquisa"]) )
 {
-    
+      //Se a variavel estiver vazia executa aqui     
     $pesquisa = "";
     $pesquisa = $_GET["pesquisa"];
 
-    if( empty($pesquisa) )
-    {
-       //Se a variavel estiver vazia executa aqui 
-       include "conexao.php";
-       $sql = "Select ID, Nome from Professor order by Id desc";
-       $resultado = $conexao->query($sql);
-       
-       $conexao->close();
+      include "conexao.php";
+      $sql = "Select ID, Nome from Professor order by Id desc";
+      $resultado = $conexao->query($sql);
+      
+      $conexao->close();
 
-    }
-    else
-    {
-        $pesquisa = "";
-    
-        //Aqui vai  Pesquisa Sa lógica da pesquisa
-    }
+}else
+{
+    //Pesquisar o Professor ligado com o Aluno 
+
+    $pesquisa = "";
 }
 
 ?>
@@ -48,8 +43,6 @@ if( isset($_GET["pesquisa"]) )
                                         value="<?php echo $pesquisa; ?>"  
                                         class="form-control" 
                                         placeholder="Digite sua pesquisa aqui..." />
-
-
                                 <button class="btn btn-primary" type="submit">
                                     Pesquisar
                                 </button>
@@ -58,39 +51,34 @@ if( isset($_GET["pesquisa"]) )
                     </div>
                     <div class="col-2"></div>
                 </div>
-
                 <div class="row">
                     <div class="col-12">
                         
                         <table class="table table-hover table-striped">
                         <thead>
                             <tr>
-                                <th scope="col">Id</th>
-                                <th scope="col">Descrição</th>
-                                <th scope="col">Valor</th>
-                                <th scope="col">Código de barras</th>
-                                <th scope="col">Imagem</th>
-                                <th scope="col"></th>
+                                <th scope="col">Nome</th>
+                                <th scope="col">Disciplina</th>
+                                <th scope="col">Atividades</th>
+                                <th scope="col">Contato</th>
                             </tr>
                         </thead>
                         <tbody>
                             <?php 
-                                if ($resultado->num_rows > 0) {
-                                    while($row = $resultado->fetch_assoc()) {
-                                        echo "<tr>";
-                                        echo "<td>" . $row["Id"] . "</td>";
-                                        echo "<td>" . $row["Descricao"] . "</td>";
-                                        echo "<td>" . $row["Valor"] . "</td>";
-                                        echo "<td>" . $row["Codigo_barras"] . "</td>";
-                                        echo "<td>" . $row["Imagem"] . "</td>";
-                                        echo "<td>" . $row["Nome"] . "</td>";                                        
-                                        echo "<td><a href='editar_produto.php?Id=$row[Id]' class='btn btn-warning' >Editar</a>  ";
-                                        echo "<a href='excluir_produto.php?Id=$row[Id]' class='btn btn-danger'>Excluir</a></td>";
-                                        echo "</tr>";
-                                    }
-                                } else {
-                                    echo "<tr><td colspan='3'>Nenhum registro encontrado</td></tr>";
-                                }
+ //                               if ($resultado->num_rows > 0) {
+   //                                 while($row = $resultado->fetch_assoc()) {
+     //                                   echo "<tr>";
+       //                                 echo "<td>" . $row["Nome"] . "</td>";
+         //                               echo "<td>" . $row["Disciplina"] . "</td>";
+           //                             echo "<td>" . $row["Atividades"] . "</td>";
+             //                           echo "<td>" . $row["Contato"] . "</td>";                                     
+                   //                     echo "<td><a href='editar_produto.php?Id=$row[Id]' class='btn btn-warning' >Editar</a>  ";
+                     //                   echo "<a href='excluir_produto.php?Id=$row[Id]' class='btn btn-danger'>Excluir</a></td>";
+                       //                 echo "</tr>";
+                  //                  }
+                    //            } else {
+                      //              echo "<tr><td colspan='3'>Nenhum registro encontrado</td></tr>";
+                        //        }
                             ?>
                                                     
                         </tbody>
@@ -102,4 +90,3 @@ if( isset($_GET["pesquisa"]) )
         </div>
     </div>
 </div>
-<?php include "rodape.php"; ?>
--- cabecario.php
+++ cabecario.php
@@ -4,12 +4,11 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Página inicial do site</title>
+    <title>SOS DS</title>
     <link href="bootstrap.min.css" rel="stylesheet"/>
 </head>
-<body>
-  
-<nav class="navbar bg-dark navbar-expand-lg bg-body-tertiary" data-bs-theme="dark">
+<body style="background: linear-gradient(90deg,black,red)">
+<nav class="navbar bg-dark navbar-expand-lg bg-body-tertiary" data-bs-theme="dark" style="background: linear-gradient(90deg,black,red)">
   <div class="container-fluid">
     <a class="navbar-brand" href="#">SOS DS</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -36,62 +35,7 @@
             <li><hr class="dropdown-divider"></li>
             <li><a class="dropdown-item" href="foreach.php">Foreach</a></li>
           </ul>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="bootstrap.php">Bootstrap</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link"  href="array.php">Array</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link"  href="sorteio.php">Sorteio</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link"  href="recupecao.php">Recuperação</a>
-        </li>
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Produtos
-          </a>
-          <ul class="dropdown-menu">
-            <li><a class="dropdown-item" href="produtos.php">Produto</a></li>
-            <li><a class="dropdown-item" href="novo_produto.php">Novo Produto</a></li>
-            <li><a class="dropdown-item" href="editar_produto.php">Editar Produto</a></li>
-          </ul>
-        </li>
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Categorias
-          </a>
-          <ul class="dropdown-menu">
-            <li><a class="dropdown-item" href="categorias.php">Categorias</a></li>
-            <li><a class="dropdown-item" href="nova_categoria.php">Nova Categoria</a></li>
-          </ul>
-        </li>
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Usuarios
-          </a>
-          <ul class="dropdown-menu">
-            <li><a class="dropdown-item" href="usuarios.php">Usuarios</a></li>
-            <li><a class="dropdown-item" href="novo_usuario.php">Novo Usuario</a></li>
-          </ul>
-        </li>
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Permissão
-          </a>
-          <ul class="dropdown-menu">
-            <li><a class="dropdown-item" href="permissao.php">Permissão</a></li>
-            <li><a class="dropdown-item" href="nova_permissao.php">Cadastra nova Permissão</a></li>
-          </ul>
-        </li>
-      </ul>
-      <form class="d-flex" role="search">
-        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-        <button class="btn btn-outline-success" type="submit">Search</button>
-      </form>
-    </div>
+</div>
   </div>
 </nav>
     <div class="container">
